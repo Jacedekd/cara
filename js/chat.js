e73c48422282048c077a1e928127246598cb66c8
Aziz <azizkazanbaev@gmail.com>
2023-01-03 21:59:13 +0600

    commit

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,78 @@
+// Create chatBot in document window
+
+let body = document.querySelector('body');
+
+let chat_bot = document.createElement('div');
+chat_bot.classList.add('chat-bar-collapsible');
+chat_bot.innerHTML = `
+<button id="chat-button" type="button" class="collapsible">
+  <div class="chat-button-Avatar">
+    <div class="chatAvatar"><img src="./img/chatbot/chatAvatar.JPG" alt=""></div>
+    
+    <div class="chat-button-Avatar-Block">
+      <p>Александра <span><i
+      id="chat-icon"
+      style="color: #fff"
+      class="fa fa-fw fa-comments-o"></i></span></p>
+      <p>Cпециалист клиентской службы</p>
+    </div>
+    <div class="chat-Icon">
+      <img src="./img/chatbot/downArrow.png" alt="">
+    </div>
+  </div>
+  
+  </button>
+<div class="content">
+<div class="full-chat-block">
+
+  <div class="outer-container">
+    <div class="chat-container">
+      <div id="chatbox">
+        <h5 id="chat-timestamp"></h5>
+        <p id="botStarterMessage" class="botText">
+          <span>Loading...</span>
+        </p>
+      </div>
+
+      <div class="chat-bar-input-block">
+        <div id="userInput">
+          <input
+            id="textInput"
+            class="input-box"
+            type="text"
+            name="msg"
+            placeholder="Нажмите 'Enter' чтобы отправить"
+          />
+          <p></p>
+        </div>
+        <div class="chat-bar-icons">
+          <i
+            id="chat-icon"
+            style="color: crimson"
+            class="fa fa-fw fa-heart"
+            onclick="heartButton()"
+          ></i>
+          <i
+            id="chat-icon"
+            style="color: #333"
+            class="fa fa-fw fa-send"
+            onclick="sendButton()"
+          ></i>
+        </div>
+      </div>
+      <div id="chat-bar-bottom">
+        <p></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`
+body.append(chat_bot);
+
+
+
+
 // Collapsible
 var coll = document.getElementsByClassName("collapsible");
 
@@ -35,7 +110,7 @@ function getTime() {
 // Получает первое сообщение
 
 function firstBotMessage() {
-  let firstMessage = "How's it going?";
+  let firstMessage = "Здравствуйте! У вас есть вопрос? <br> Или вам нужна помощь?";
   document.getElementById("botStarterMessage").innerHTML =
     '<p class="botText"><span>' + firstMessage + "</span></p>";
 
@@ -61,7 +136,7 @@ function getResponse() {
   let userText = $("#textInput").val();
 
   if (userText == "") {
-    userText = "Thank you for donate!";
+    userText = "Здравствуйте";
   }
 
   let userHtml = '<p class="userText"><span>' + userText + "</span></p>";
@@ -108,13 +183,20 @@ function getBotResponse(input) {
   } else if (input == "how i can connact with hr manager?") {
     return "You can write to our mailing address: abc@gmail.com";
   } else {
-    return "Try asking something else!";
+    return "Мы ответим вам в ближайшее время)";
   }
 }
 
 
 let content = document.querySelector('.content');
 
-window.addEventListener('scroll', () => {
-  content.style = 'max-height: 0;'
-})
+// window.addEventListener('scroll', () => {
+//   if (content.style.maxHeight = '450px') {
+//     content.style = 'maxHeight: 0;';
+//     coll[0].classList.remove('active');
+//   }
+// })
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -14,69 +14,18 @@ if (close) {
 }
 
 
-let body = document.querySelector('body');
+const MainImg = document.getElementById("MainImg");
+const smallimg = document.getElementsByClassName("small-img");
 
-let chat_bot = document.createElement('div');
-chat_bot.classList.add('chat-bar-collapsible');
-chat_bot.innerHTML = `
-<button id="chat-button" type="button" class="collapsible">
-<div class="chat-button-Avatar">
-  <div class="chatAvatar"><img src="./img/chatbot/chatAvatar.JPG" alt=""></div>
-  
-  <div class="chat-button-Avatar-Block">
-    <p>Александра <span><i
-    id="chat-icon"
-    style="color: #fff"
-    class="fa fa-fw fa-comments-o"></i></span></p>
-    <p>Cпециалист клиентской службы</p>
-  </div>
-</div>
-
-</button>
-<div class="content">
-<div class="full-chat-block">
-
-  <div class="outer-container">
-    <div class="chat-container">
-      <div id="chatbox">
-        <h5 id="chat-timestamp"></h5>
-        <p id="botStarterMessage" class="botText">
-          <span>Loading...</span>
-        </p>
-      </div>
-
-      <div class="chat-bar-input-block">
-        <div id="userInput">
-          <input
-            id="textInput"
-            class="input-box"
-            type="text"
-            name="msg"
-            placeholder="Tap 'Enter' to send a message"
-          />
-          <p></p>
-        </div>
-        <div class="chat-bar-icons">
-          <i
-            id="chat-icon"
-            style="color: crimson"
-            class="fa fa-fw fa-heart"
-            onclick="heartButton()"
-          ></i>
-          <i
-            id="chat-icon"
-            style="color: #333"
-            class="fa fa-fw fa-send"
-            onclick="sendButton()"
-          ></i>
-        </div>
-      </div>
-      <div id="chat-bar-bottom">
-        <p></p>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-`
-body.append(chat_bot);
+smallimg[0].onclick = function () {
+  MainImg.src = smallimg[0].src;
+};
+smallimg[1].onclick = function () {
+  MainImg.src = smallimg[1].src;
+};
+smallimg[2].onclick = function () {
+  MainImg.src = smallimg[2].src;
+};
+smallimg[3].onclick = function () {
+  MainImg.src = smallimg[3].src;
+};
